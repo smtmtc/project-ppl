2711cf35a71b45c32df9b1f088a196b7090418f7
smtmtc <smtmtc@gmail.com>
2022-05-17 19:43:30 +0700

    menambahkan tabel

--- admin/manajemen.php
+++ admin/manajemen.php
@@ -8,7 +8,7 @@
     <meta name="author" content="" />
     <title>Dashboard - Admin</title>
     <link href="https://cdn.jsdelivr.net/npm/simple-datatables@latest/dist/style.css" rel="stylesheet" />
-    <link href="css/styles.css" rel="stylesheet" />
+    <link href="../css/styles.css" rel="stylesheet" />
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css">
     <script src="https://use.fontawesome.com/releases/v6.1.0/js/all.js" crossorigin="anonymous"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous" />
@@ -28,14 +28,39 @@
           <a class="nav-item nav-link" href="distribusi.php">Distribusi</a>
         </div>
     </nav>
-    <h1>HALAMAN MANAJEMEN</h1>
-    <a href="../admin.php">back</a>
+    <div id="layoutSidenav" style="background-color: #00FF7F;">
+      <div id="layoutSidenav_content">
+          <main class="my-4 mx-auto" style="width: 1000px;">
+              <div class="container-fluid px-4">
+              <h1 class="text-center align-items-center">HALAMAN MANAJEMEN</h1>
+                  <div class="row justify-content-center">
+                    <div class="col-xl-4 col-md-4 col-sm-6">
+                        <div class="card bg-light mb-4">
+                            <div class="card-body">
+                              <h5 class="d-flex justify-content-center"><a href="manajemen/m_produksi.php" class="stretched-link"></a> Manajemen Produksi</h5>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-xl-4 col-md-4 col-sm-6">
+                        <div class="card bg-light mb-4">                                    
+                            <div class="card-body">
+                              <h5 class="d-flex justify-content-center"><a href="manajemen/m_keuangan.php" class="stretched-link"></a> Manajemen Keuangan</h5>
+                            </div>
+                        </div>
+                    </div>
+                  </div>
+              </div>
+          </main>
+      </div>
+    </div>
+
+    </div>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
-        <script src="js/scripts.js"></script>
+        <script src="../js/scripts.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.min.js" crossorigin="anonymous"></script>
         <script src="assets/demo/chart-area-demo.js"></script>
         <script src="assets/demo/chart-bar-demo.js"></script>
         <script src="https://cdn.jsdelivr.net/npm/simple-datatables@latest" crossorigin="anonymous"></script>
-        <script src="js/datatables-simple-demo.js"></script>
+        <script src="../js/datatables-simple-demo.js"></script>
   </body>
 </html>
--- admin/manajemen/m_keuangan.php
+++ admin/manajemen/m_keuangan.php
@@ -8,58 +8,68 @@
     <meta name="author" content="" />
     <title>Dashboard - Admin</title>
     <link href="https://cdn.jsdelivr.net/npm/simple-datatables@latest/dist/style.css" rel="stylesheet" />
-    <link href="../css/styles.css" rel="stylesheet" />
+    <link href="../../css/styles.css" rel="stylesheet" />
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css">
     <script src="https://use.fontawesome.com/releases/v6.1.0/js/all.js" crossorigin="anonymous"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous" />
   </head>
-  <body>
+  <body class="">
     <nav class="sb-topnav navbar navbar-expand navbar-dark bg-dark d-flex justify-content-between">
         <!-- Navbar Brand-->
         <a class="navbar-brand ps-3" href="../admin.php">Tempe Bu Asri</a>
         <!-- Sidebar Toggle-->
         <div class="navbar-nav ">
-          <a class="nav-item nav-link" href="../admin.php">Home</a>
-          <a class="nav-item nav-link active" href="penjualan.php">Penjual<span class="sr-only">(current)</span></a>
-          <a class="nav-item nav-link" href="manajemen.php">Manajemen</a>
-          <a class="nav-item nav-link" href="bahan_baku.php">Bahan Baku</a>
-          <a class="nav-item nav-link" href="pembayaran.php">Pembayaran</a>
-          <a class="nav-item nav-link" href="pembelian.php">Pembelian</a>
-          <a class="nav-item nav-link" href="distribusi.php">Distribusi</a>
+          <a class="nav-item nav-link" href="../../admin.php">Home</a>
+          <a class="nav-item nav-link" href="../penjualan.php">Penjualan</a>
+          <a class="nav-item nav-link active" href="../manajemen.php">Manajemen <span class="sr-only">(current)</span></a>
+          <a class="nav-item nav-link" href="../bahan_baku.php">Bahan Baku</a>
+          <a class="nav-item nav-link" href="../pembayaran.php">Pembayaran</a>
+          <a class="nav-item nav-link" href="../pembelian.php">Pembelian</a>
+          <a class="nav-item nav-link" href="../distribusi.php">Distribusi</a>
         </div>
     </nav>
     <div id="layoutSidenav" style="background-color: #00FF7F;">
       <div id="layoutSidenav_content">
           <main class="my-4 mx-auto" style="width: 1000px;">
               <div class="container-fluid px-4">
-              <h1 class="text-center">HALAMAN PENJUAL</h1>
+              <h1 class="text-center">HALAMAN MANAJEMEN</h1>
+                  <div class="row justify-content-center">
+                    <div class="col-xl-4 col-md-4 col-sm-6">
+                        <div class="card bg-light mb-4">
+                            <div class="card-body">
+                              <h5 class="d-flex justify-content-center"><a href="m_produksi.php" class="stretched-link"></a> Manajemen Produksi</h5>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-xl-4 col-md-4 col-sm-6">
+                        <div class="card bg-light mb-4">                                    
+                            <div class="card-body">
+                              <h5 class="d-flex justify-content-center"><a href=".m_keuangan.php" class="stretched-link"></a> Manajemen Keuangan</h5>
+                            </div>
+                        </div>
+                    </div>
+                  </div>
                   <form class="bg-light px-5 py-5">
                     <div class="row mb-3">
-                        <label for="inputPassword3" class="col-sm-2 col-form-label">Nama</label>
+                        <label for="inputEmail3" class="col-sm-2 col-form-label">Tanggal Produksi</label>
+                        <div class="col-sm-10">
+                        <input type="email" class="form-control" id="inputEmail3">
+                        </div>
+                    </div>
+                    <div class="row mb-3">
+                        <label for="inputPassword3" class="col-sm-2 col-form-label">Tanggal Kadaluarsa</label>
                         <div class="col-sm-10">
                         <input type="password" class="form-control" id="inputPassword3">
                         </div>
                     </div>
                     <div class="row mb-3">
-                        <label for="inputPassword3" class="col-sm-2 col-form-label">Tempat</label>
+                        <label for="inputPassword3" class="col-sm-2 col-form-label">Banyak</label>
                         <div class="col-sm-10">
                         <input type="password" class="form-control" id="inputPassword3">
                         </div>
                     </div>
                     <div class="row mb-3">
-                        <label for="inputPassword3" class="col-sm-2 col-form-label">Tanggal Lahir</label>
-                        <div class="col-sm-10">
-                        <input type="password" class="form-control" id="inputPassword3">
-                        </div>
-                    </div>
-                    <div class="row mb-3">
-                        <label for="inputPassword3" class="col-sm-2 col-form-label">Usia</label>
-                        <div class="col-sm-10">
-                        <input type="password" class="form-control" id="inputPassword3">
-                        </div>
-                    </div>
-                    <div class="row mb-3">
-                        <label for="inputPassword3" class="col-sm-2 col-form-label">Bagian</label>
+                        <label for="inputPassword3" class="col-sm-2 col-form-label">Jenis Tempe</label>
                         <div class="col-sm-10">
                         <input type="password" class="form-control" id="inputPassword3">
                         </div>
@@ -69,7 +79,7 @@
                       <button type="submit" class="btn btn-primary mx-2">Tambah Data</button>
                     </div>
                   </form>
-                  <div class="card mb-4 mt-4">
+                  <div class="card my-4">
                       <div class="card-header">
                           <i class="fas fa-table me-1"></i>
                           DataTable Example
@@ -78,11 +88,11 @@
                           <table id="datatablesSimple">
                               <thead>
                                   <tr>
-                                      <th>Nama</th>
-                                      <th>Tempat</th>
-                                      <th>Tanggal Lahir</th>
-                                      <th>Usia</th>
-                                      <th>Bagian</th>
+                                      <th>No.</th>
+                                      <th>Tanggal produksi</th>
+                                      <th>Tanggal kadaluarsa</th>
+                                      <th>Banyak</th>
+                                      <th>Jenis tempe</th>
                                       <th>Aksi</th>
                                   </tr>
                               </thead>
@@ -201,12 +211,14 @@
           </main>
       </div>
     </div>
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
-  <script src="../js/scripts.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.min.js" crossorigin="anonymous"></script>
-  <script src="assets/demo/chart-area-demo.js"></script>
-  <script src="assets/demo/chart-bar-demo.js"></script>
-  <script src="https://cdn.jsdelivr.net/npm/simple-datatables@latest" crossorigin="anonymous"></script>
-  <script src="../js/datatables-simple-demo.js"></script>
+
+    </div>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+        <script src="../../js/scripts.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.min.js" crossorigin="anonymous"></script>
+        <script src="assets/demo/chart-area-demo.js"></script>
+        <script src="assets/demo/chart-bar-demo.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/simple-datatables@latest" crossorigin="anonymous"></script>
+        <script src="../../js/datatables-simple-demo.js"></script>
   </body>
 </html>
